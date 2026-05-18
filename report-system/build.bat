ec440dc5b7844ebaced32336be906725fafc0d11
Dominika Smorąg <dominikasmorag@gmail.com>
2026-05-18 11:15:04 +0200

    removed building spring app from Dockerfile separately - its built within docker-compose now

--- report-system/build.bat
+++ report-system/build.bat
@@ -13,18 +13,6 @@ call .\mvnw clean package -DskipTests
 
 echo.
 echo =========================
-echo REMOVING OLD IMAGE
-echo =========================
-docker rmi spring-app
-
-echo.
-echo =========================
-echo BUILDING DOCKER IMAGE
-echo =========================
-docker build -t spring-app .
-
-echo.
-echo =========================
 echo STARTING DOCKER COMPOSE
 echo =========================
 docker compose up -d --build
