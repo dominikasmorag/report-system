d391ea687714b61aba34906a63ac239769a81bb8
Dominika Smorąg <dominikasmorag@gmail.com>
2026-05-19 11:41:45 +0200

    Mockito tests

--- report-system/src/test/java/com/ds/report_system/service/UserServiceTest.java
+++ report-system/src/test/java/com/ds/report_system/service/UserServiceTest.java
@@ -1,6 +1,9 @@
 package com.ds.report_system.service;
 
 import com.ds.report_system.entity.UserEntity;
+import com.ds.report_system.exceptions.user.EmailAlreadyExistsException;
+import com.ds.report_system.exceptions.user.UsernameAlreadyExistsException;
+import com.ds.report_system.pojo.Role;
 import com.ds.report_system.pojo.UserRequest;
 import com.ds.report_system.pojo.UserResponse;
 import com.ds.report_system.repository.UserRepository;
@@ -12,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -47,6 +51,29 @@ public class UserServiceTest {
 
         assertEquals("ania", result.getUsername());
         assertEquals("ania@gmail.com", result.getEmail());
+    }
 
+    @Test
+    void shouldThrowEmailAlreadyExistsException() {
+        UserRequest userRequest = new UserRequest("ania", "ania123", "ania@gmail.com");
+
+        when(repository.existsByEmail("ania@gmail.com")).thenReturn(true);
+
+        assertThrows(
+                EmailAlreadyExistsException.class,
+                () -> service.register(userRequest)
+        );
+    }
+
+    @Test
+    void shouldThrowUserAlreadyExistsException() {
+        UserRequest userRequest = new UserRequest("ania", "ania1234", "ania@gmail.com");
+
+        when(repository.existsByUsername("ania")).thenReturn(true);
+
+        assertThrows(
+                UsernameAlreadyExistsException.class,
+                () -> service.register(userRequest)
+        );
     }
 }
