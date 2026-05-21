f4e9c913b46d66ca5c9aa187c4bc4b270a104007
Dominika Smorąg <dominikasmorag@gmail.com>
2026-05-21 13:15:04 +0200

    mapper -> userEntity -> toDto

--- report-system/src/main/java/com/ds/report_system/mapper/UserMapper.java
+++ report-system/src/main/java/com/ds/report_system/mapper/UserMapper.java
@@ -1,4 +1,20 @@
 package com.ds.report_system.mapper;
 
+import com.ds.report_system.dto.user.UserResponse;
+import com.ds.report_system.entity.UserEntity;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+
+    public UserMapper() {}
+
+    public UserResponse toDto(UserEntity userEntity) {
+        return new UserResponse(
+                userEntity.getId(),
+                userEntity.getUsername(),
+                userEntity.getRole().name(),
+                userEntity.getEmail()
+        );
+    }
 }
