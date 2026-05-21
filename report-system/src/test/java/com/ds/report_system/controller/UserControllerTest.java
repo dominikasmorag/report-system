c1da3fba87b0cf26d60beab36500171e306f3445
Dominika Smorąg <dominikasmorag@gmail.com>
2026-05-21 21:44:56 +0200

    UserControllerTest class created
    Co-authored-by: dominikasmorag <dominikasmorag@gmail.com>

---
+++ report-system/src/test/java/com/ds/report_system/controller/UserControllerTest.java
@@ -0,0 +1,8 @@
+package com.ds.report_system.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UserControllerTest {
+}
