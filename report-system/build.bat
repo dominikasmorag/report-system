93be2d94db2e2b196154318ad3a94e5085bfb09f
Dominika Smorąg <dominikasmorag@gmail.com>
2026-05-23 19:57:45 +0200

    docker prune to avoid too many images

--- report-system/build.bat
+++ report-system/build.bat
@@ -7,18 +7,32 @@ docker compose down
 
 echo .
 echo =========================
+echo DOCKER PRUNE
+echo =========================
+docker image prune -a
+
+echo .
+echo =========================
 echo BUILDING MAVEN PROJECT
 echo =========================
 call .\mvnw clean package
 
 echo.
 echo =========================
-echo STARTING DOCKER COMPOSE
+echo BUILDING DOCKER APP IMAGE
 echo =========================
-docker build -t spring-app .
+docker build -t backend-image .
+
+echo.
+echo =========================
+echo BUILDING DOCKER NGINX IMAGE
+echo =========================
+cd nginx
+docker build -t nginx-image .
 
 echo.
 echo =========================
 echo STARTING DOCKER COMPOSE
 echo =========================
+cd ..
 docker compose up -d --build
